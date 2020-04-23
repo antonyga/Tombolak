a6e04068d79b7b7e09743e5b29af8b8300c8a6cd
Antony G <antonyg.2402@gmail.com>
2020-04-24 00:00:34 +0200

    modified:   app.js 	modified:   index.html     -m "functional code"

--- app.js
+++ app.js
@@ -1,33 +1,66 @@
-// Preload .txt file with list of tasks
 
-function Preload(){
-    raw = loadStrings('tasks.txt');
+
+let attendeesArray = [];
+
+function handleFiles(files){
+    if(window.handleFiles){
+        getAsText(files[0]);
+        fileUploaded = true;
+    }else{
+        alert('Filereader not supported in this browser.');
+    }
+}
+
+function getAsText(fileToRead){
+    var reader = new FileReader();
+    // Handle load errors
+    reader.onload = loadHandler;
+    reader.onerror = errorHandler;
+    // Read the file into memory as UFT-8
+    reader.readAsText(fileToRead);
+}
+
+function loadHandler(event){
+    var csv = event.target.result;
+    processData(csv)
+}
+
+function processData(csv){
+    var allTextLines = csv.split('\n'); 
+    attendeesArray = allTextLines;
     
 }
 
-function setup(){
-    noCanvas();
-    let sdf = raw[0];
-    console.log(sdf);
+
+function errorHandler(evt) {
+	if(evt.target.error.name == "NotReadableError") {
+		alert("Cannot read file !");
+	}
 }
 
 
-//var button = document.getElementById('rndBtn');
 
 
-//button.onclick = function randomTask (){    
+const randomizerWinner = ()=>{
     
-    /*const selectedTaskss = document.querySelector('.task p')
-    const task = ['HelloWorld', 'do-while', 'sample of a boolean'];
-    const computerNumber = Math.floor(Math.random() * 3); 
-    const lastChoice = task[computerNumber];   
-    selectedTaskss.textContent = (`Your task is: ${lastChoice}`);
-    console.log(computerNumber);*/  
-    //tasksList = loadStrings('tasks.txt');
-    //let roro = tasksList[0];
-    //console.log(roro); 
-    
-//}
+    const task = document.querySelector('.task p');
+    let min = 0;
+    let max = attendeesArray.length - 1;
 
-Preload();
-setup();
+    // Get a random number from total items in array. Let's say the array has 10 items.
+    // This line below chose a random number between 0 and 9.
+    let winnerIndex = Math.floor(Math.random() * (max - min));
+    
+    // in the line below we set the choosen number to an item position in the array attendeesArray.
+    // Lets say the array contains 3 items ('Mary', 'Sussy', 'Pedro'). If the chosen number is 0
+    // the variable winner will be set to 'Mary'.
+    let winner = attendeesArray[winnerIndex];
+    console.log(winner);
+    
+
+    task.textContent = winner;
+
+
+    
+}
+document.querySelector('.randomBtn button').addEventListener('click', randomizerWinner);
