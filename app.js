14acb3926b30d5b9024eeebe98ab3745c2b15424
Antony G <antonyg.2402@gmail.com>
2020-04-26 19:05:45 +0200

    Code commenting and organization

--- app.js
+++ app.js
@@ -1,50 +1,37 @@
-
-
 let attendeesArray = [];
-let fileUploaded;
 
 function handleFiles(files){
-    
     if(window.handleFiles){
         getAsText(files[0]);
-        fileUploaded = true;
-    }else{
-        alert('Filereader not supported in this browser.');
     }
-}
-
-checkfile(sender){
-    var validExts = new Array('.xlsx', '.xls', '.csv');
-    var fileExt = sender.value;
-    fileExt = fileExt.substring(fileExt.lastIndexOf('.'));
-    if(validExts.indexOf(fileExt) < 0){
-        alert('not a valid file type')
+    else{
+        alert('fileReader not supported in this browser.');
     }
 }
 
 function getAsText(fileToRead){
-    var reader = new FileReader();
-    
+    var reader = new FileReader();    
     // Read the file into memory as UFT-8
     reader.readAsText(fileToRead);
 
-    // Handle load errors
     reader.onload = loadHandler;
-    reader.onerror = errorHandler;
-    
+
+    // Handle load errors
+    reader.onerror = errorHandler;    
 }
 
 function loadHandler(event){
-    var csv = event.target.result;
-    processData(csv)
+    var csv = event.target.result; 
+    processData(csv);  
 }
 
 function processData(csv){
-    var allTextLines = csv.split('\n'); 
-    attendeesArray = allTextLines;
+    // Split the csv array to separate each task in the list.
+    var allTextLines = csv.split('\n');
     
+    // Pass the array
+    attendeesArray = allTextLines;    
 }
-
 
 function errorHandler(evt) {
 	if(evt.target.error.name == "NotReadableError") {
@@ -52,11 +39,7 @@ function errorHandler(evt) {
 	}
 }
 
-
-
-
-const randomizerWinner = ()=>{
-    
+const randomizerWinner = ()=>{    
     const task = document.querySelector('.task p');
     let min = 0;
     let max = attendeesArray.length - 1;
@@ -65,13 +48,12 @@ const randomizerWinner = ()=>{
     // This line below chose a random number between 0 and 9.
     let winnerIndex = Math.floor(Math.random() * (max - min));
     
-    // in the line below we set the choosen number to item position in the array attendeesArray.
+    // In the line below we set the choosen number to an item position in the array attendeesArray.
     // Lets say the array contains 3 items ('Mary', 'Sussy', 'Pedro'). If the chosen number is 0
     // the variable winner will be set to 'Mary'.
     let winner = attendeesArray[winnerIndex];
-    
-    console.log(winner);
-    
+
+    // Pass the choosen task to the HTML, so user can visualize it.
     task.textContent = winner;    
 }
 
